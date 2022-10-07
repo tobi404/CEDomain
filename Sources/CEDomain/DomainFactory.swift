be5346d8fb2a545c4292f05742455a8fdc1a3950
Beka Demuradze <demuradze@gmail.com>
2022-10-07 12:16:28 +0400

    Mock registering

--- Sources/CEDomain/DomainFactory.swift
+++ Sources/CEDomain/DomainFactory.swift
@@ -18,7 +18,15 @@ public class Dependencies {
 }
 
 public protocol CoreContainerInterface {
+    func registerMocks()
+
     // Modules
     var userBalanceDataRepository: Factory<UserBalanceDataInterface> { get }
     var currencyConverterDataRepository: Factory<CurrencyConverterDataInterface> { get }
+}
+
+public extension Container {
+    static func registerMocks() {
+        Dependencies.core.registerMocks()
+    }
 }
