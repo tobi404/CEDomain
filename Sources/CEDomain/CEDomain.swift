83008b612294c13a8747a3badb65ffa1b431ae64
Beka Demuradze <demuradze@gmail.com>
2022-10-07 10:44:56 +0400

    Initial Commit

---
+++ Sources/CEDomain/CEDomain.swift
@@ -0,0 +1,6 @@
+public struct CEDomain {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CEDomainTests/CEDomainTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CEDomain
+
+final class CEDomainTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CEDomain().text, "Hello, World!")
+    }
+}
